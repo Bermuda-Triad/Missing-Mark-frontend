93701eb590e17e6096122821c1b6a057c3515cb7
sp <sparks@localhost.localdomain>
2023-10-05 14:58:23 +0300

    color

--- src/components/authpage/Login.tsx
+++ src/components/authpage/Login.tsx
@@ -1,104 +1,125 @@
-import React, { ChangeEvent, useReducer } from 'react'
-import { useState } from 'react'
-
-
+import React, { ChangeEvent, useReducer } from "react";
+import { useState } from "react";
 
 //validation
-import { validation } from './validations/Validations'
-import { Link, useNavigate } from 'react-router-dom'
+import { validation } from "./validations/Validations";
+import { Link, useNavigate } from "react-router-dom";
 
-
-type Props = {}
+type Props = {};
 
 type Validation = {
-  validate:(inputs:string) => boolean
-}
+  validate: (inputs: string) => boolean;
+};
 type Inputs = {
-  username:string;
-  password:string;
-  confirmPassword:string;
-}
+  username: string;
+  password: string;
+  confirmPassword: string;
+};
 
 type State = {
-  userName:String;
-  password:string;
-}
+  userName: String;
+  password: string;
+};
 
 type Action =
-  | { type: 'SET_USERNAME'; payload: string }
-  | { type: 'SET_PASSWORD'; payload: string };
+  | { type: "SET_USERNAME"; payload: string }
+  | { type: "SET_PASSWORD"; payload: string };
 
-
-  // this is the reducer that will be used for validation
-  // basically usereducer takes in to input
-  // initial and the dispatch ie to call the function
-
+// this is the reducer that will be used for validation
+// basically usereducer takes in to input
+// initial and the dispatch ie to call the function
 
 function Login({}: Props) {
+  const navigate = useNavigate();
 
-  const navigate = useNavigate()
+  let vaStudent = new validation();
 
-  let vaStudent = new validation
+  const [username, setUserName] = useState<Inputs["username"]>("");
+  const [input, setInput] = useState<boolean>(true);
+  const [password, setPassword] = useState<Inputs["password"]>();
+  const [confirmPassword, setConfirmPassword] =
+    useState<Inputs["confirmPassword"]>();
 
-    const [username,setUserName] = useState<Inputs["username"] >("")
-    const [input,setInput] = useState<boolean>(true)
-    const [password,setPassword] = useState<Inputs["password"]>()
-    const [confirmPassword,setConfirmPassword] = useState<Inputs["confirmPassword"]>()
-    
-
-    let checker = {
-      valid:'border-b border-defaultgray',
-      invalid:'  border-b border-red'
-    }
+  let checker = {
+    valid: "border-b border-defaultgray",
+    invalid: "  border-b border-red",
+  };
   //  validation
-  function refReg(e){
+  function refReg(e) {
     // setInput(vaStudent.validateStudent(username as string));
     // if the length si valid
-    setUserName(e.target.value)
-   
-    
-    if(username.length > 8) {
-      setInput(true) 
-      
-      console.log(username.length)
+    setUserName(e.target.value);
 
+    if (username.length > 8) {
+      setInput(true);
+
+      console.log(username.length);
     }
-  //  else if(username.length  <3) setInput(true)
-   else {
-    setInput(false)
-   }
-    console.log(input)
-    console.log(username)
+    //  else if(username.length  <3) setInput(true)
+    else {
+      setInput(false);
+    }
+    console.log(input);
+    console.log(username);
   }
-  
 
   return (
-    <div className='w-[432px] '>
-      <div className='mb-[55px]'>
-      <h2 className='text-[36px] text-secondary font-[800] mb-[11px] font-Manrope'>Login</h2>
-      <p className='text-[13.33px] text-defaultgray italic'>This only applies to students of Masinde Muliro University of Science and Techology</p>
+    <div className="w-[432px] ">
+      <div className="mb-[55px]">
+        <h2 className="text-[36px] text-secondary font-[800] mb-[11px] font-Manrope">
+          Login
+        </h2>
+        <p className="text-[13.33px] text-defaultgray italic">
+          This only applies to students of Masinde Muliro University of Science
+          and Techology
+        </p>
       </div>
 
-      <form action="" className='flex flex-col gap-[36px]'>
-      
-
-        <span className={`border-b border-defaultgray outline-none ${ input ? checker.valid : checker.invalid}` }><input type="password" className={` outline-none placeholder-defaultgray   text-secondary text-[19.2px] w-full `}  placeholder='RF Number/ Reg Number' onChange={refReg}/>  </span>
-        <span className='border-b border-defaultgray outline-none '><input type="password" className='border-none outline-none text-secondary placeholder-defaultgray  text-[19.2px] '  placeholder='Password'/>  </span>
+      <form action="" className="flex flex-col gap-[36px]">
+        <span
+          className={`border-b border-defaultgray outline-none ${
+            input ? checker.valid : checker.invalid
+          }`}
+        >
+          <input
+            type="password"
+            className={` outline-none   text-secondary text-[19.2px] w-full `}
+            placeholder="RF Number/ Reg Number"
+            onChange={refReg}
+          />{" "}
+        </span>
+        <span className="border-b border-defaultgray outline-none ">
+          <input
+            type="password"
+            className="border-none outline-none   text-secondary text-[19.2px] "
+            placeholder="Password"
+          />{" "}
+        </span>
 
         {/* <input type="text" >welcome</input> */}
-       <input type="submit" value="Login" onClick={e => navigate("/student")} className='py-[10px] cursor-pointer bg-secondary text-white' />
+        <input
+          type="submit"
+          value="Login"
+          onClick={(e) => navigate("/student")}
+          className="py-[10px] cursor-pointer bg-secondary text-white"
+        />
       </form>
 
-      <p className='my-[10px] text-[16px] flex items-center justify-between'>
-       <h2>Forgot Password?  <a href="" className='text-red' >Click here</a>
+      <p className="my-[10px] text-[16px] flex items-center justify-between">
+        <h2>
+          Forgot Password?{" "}
+          <a href="" className="text-red">
+            Click here
+          </a>
+        </h2>
 
-       </h2>
-
-       <Link to="/register" className='text-red' > Register </Link>
-
-       </p>
+        <Link to="/register" className="text-red">
+          {" "}
+          Register{" "}
+        </Link>
+      </p>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
--- src/components/authpage/Regestration.tsx
+++ src/components/authpage/Regestration.tsx
@@ -1,142 +1,154 @@
-import React, { ChangeEvent, useReducer } from 'react'
-import { useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import React, { ChangeEvent, useReducer } from "react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-type Props = {}
+type Props = {};
 
 type Validation = {
-  validate:(inputs:string) => boolean
- 
-}
+  validate: (inputs: string) => boolean;
+};
 type Inputs = {
-  username:string
-  password:string
-  confirmPassword:string
-}
+  username: string;
+  password: string;
+  confirmPassword: string;
+};
 
 type State = {
-  username:string;
-  password:string;
-  confirmPassword:string;
-}
+  username: string;
+  password: string;
+  confirmPassword: string;
+};
 
 type Action =
-  | { type: 'SET_USERNAME'; payload: string }
-  | { type: 'SET_PASSWORD'; payload: string }
-  |{ type:'SET_CONFIRM_PASSWORD'; payload:string};
+  | { type: "SET_USERNAME"; payload: string }
+  | { type: "SET_PASSWORD"; payload: string }
+  | { type: "SET_CONFIRM_PASSWORD"; payload: string };
 
+// this is the reducer that will be used for validation
+// basically usereducer takes in to input
+// initial and the dispatch ie to call the function
 
-  // this is the reducer that will be used for validation
-  // basically usereducer takes in to input
-  // initial and the dispatch ie to call the function
-
-function formReducer(state:State,action:Action):State{
-
- 
-  switch(action.type){
-    case 'SET_USERNAME':
-      return{
+function formReducer(state: State, action: Action): State {
+  switch (action.type) {
+    case "SET_USERNAME":
+      return {
         ...state,
-        username :action.payload
-      }
-    case 'SET_PASSWORD':
-      return{
+        username: action.payload,
+      };
+    case "SET_PASSWORD":
+      return {
         ...state,
-        password:action.payload
-      }
-      case 'SET_CONFIRM_PASSWORD':
-        return{
-          ...state,
-          confirmPassword:action.payload
-        }
-      default:
-        return state
+        password: action.payload,
+      };
+    case "SET_CONFIRM_PASSWORD":
+      return {
+        ...state,
+        confirmPassword: action.payload,
+      };
+    default:
+      return state;
   }
 }
 
 function Regestration({}: Props) {
-    
-  const [passwordError,setPasswordError] = useState("")
-    
-    // defina the initial state
+  const [passwordError, setPasswordError] = useState("");
 
-    let initialState:State = {
-      username:"",
-      password:'',
-      confirmPassword:''
-    }
-    // the difination of the function
+  // defina the initial state
 
-    const [state,dispatch] = useReducer(formReducer,initialState)
+  let initialState: State = {
+    username: "",
+    password: "",
+    confirmPassword: "",
+  };
+  // the difination of the function
 
-    // when any input of the inputs changes it should trigger the following code
-    const navigate = useNavigate()
+  const [state, dispatch] = useReducer(formReducer, initialState);
 
-  
+  // when any input of the inputs changes it should trigger the following code
+  const navigate = useNavigate();
 
-    function handleInputChange(e:ChangeEvent<HTMLInputElement>):void{
+  function handleInputChange(e: ChangeEvent<HTMLInputElement>): void {
+    // then it should change the input
+    const { name, value } = e.target;
 
-          // then it should change the input
-          const {name,value} = e.target
-
-          dispatch({
-            type:`SET_${name.toUpperCase()}`,payload:value
-          })
-
-
-    }
-    const handleRegistration = (e)=>
-    {
-      e.preventDefault()
-      if(state.password !== state.confirmPassword) {
-        state.confirmPassword = ""
-        setPasswordError("The passwords do not match")
-    
-    }
-    else setPasswordError("")
-    console.log(passwordError)
-      console.log(state)
-      
-    }
-  
-  
+    dispatch({
+      type: `SET_${name.toUpperCase()}`,
+      payload: value,
+    });
+  }
+  const handleRegistration = (e) => {
+    e.preventDefault();
+    if (state.password !== state.confirmPassword) {
+      state.confirmPassword = "";
+      setPasswordError("The passwords do not match");
+    } else setPasswordError("");
+    console.log(passwordError);
+    console.log(state);
+  };
 
   return (
-    <div className='w-[432px] '>
-      <div className='mb-[55px]'>
-      <h2 className='text-[36px] text-secondary font-[800] mb-[11px] font-Manrope'>Registration</h2>
-      <p className='text-[13.33px] text-defaultgray italic'>This only applies to students of Masinde Muliro University of Science and Techology</p>
+    <div className="w-[432px] ">
+      <div className="mb-[55px]">
+        <h2 className="text-[36px] text-secondary font-[800] mb-[11px] font-Manrope">
+          Registration
+        </h2>
+        <p className="text-[13.33px] text-defaultgray italic">
+          This only applies to students of Masinde Muliro University of Science
+          and Techology
+        </p>
       </div>
 
-      <form action="" className='flex flex-col gap-[36px]'>
-        <span className='border-b border-defaultgray outline-none '><input 
-        value={state.username}
-        name='username'
-        onChange={handleInputChange}
-        type="text" className='border-none outline-none text-secondary text-[19.2px] '  placeholder='Reg Number'
-       
+      <form action="" className="flex flex-col gap-[36px]">
+        <span className="border-b border-defaultgray outline-none ">
+          <input
+            value={state.username}
+            name="username"
+            onChange={handleInputChange}
+            type="text"
+            className="border-none outline-none text-secondary text-[19.2px] "
+            placeholder="Reg Number"
+          />
+        </span>
 
+        <span className="border-b border-defaultgray outline-none ">
+          <input
+            type="password"
+            className="border-none outline-none text-secondary text-[19.2px] "
+            placeholder="Password"
+            name="password"
+            onChange={handleInputChange}
+          />{" "}
+        </span>
+        <span className="border-b border-defaultgray outline-none ">
+          <input
+            type="password"
+            className="border-none outline-none text-secondary  text-[19.2px] "
+            placeholder="Confirm Password"
+            name="confirm_password"
+            onChange={handleInputChange}
+          />{" "}
+        </span>
 
-        /> 
-         </span>
-
-        <span className='border-b border-defaultgray outline-none '>
-          <input type="password" className='border-none outline-none text-secondary text-[19.2px] '  placeholder='Password' name='password'  onChange={handleInputChange} />  </span>
-        <span className='border-b border-defaultgray outline-none '>
-          <input type="password" className='border-none outline-none text-secondary  text-[19.2px] '  placeholder='Confirm Password' name='confirm_password'  onChange={handleInputChange} />  </span>
-
-        <p className='text-red italic '>{passwordError}</p>
-       <input type="submit" value="Register" className='py-[10px] cursor-pointer bg-secondary text-white' onClick={
-        () => {
-          handleRegistration
-          navigate("/")
-        }
-       } />
+        <p className="text-red italic ">{passwordError}</p>
+        <input
+          type="submit"
+          value="Register"
+          className="py-[10px] cursor-pointer bg-secondary text-white"
+          onClick={() => {
+            handleRegistration;
+            navigate("/");
+          }}
+        />
       </form>
 
-      <p className='my-[10px] text-[16px]'>Already have an account?  <Link to="/"  className='text-red'  >Login</Link></p>
+      <p className="my-[10px] text-[16px]">
+        Already have an account?{" "}
+        <Link to="/" className="text-red">
+          Login
+        </Link>
+      </p>
     </div>
-  )
+  );
 }
 
-export default Regestration
+export default Regestration;
